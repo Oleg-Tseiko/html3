424998ed0bdbc3fcab37e72cf10853f9111df31a
Oleg <O.Tseiko@gmail.com>
2021-03-18 10:13:59 +0200

    added bootstrap slider

--- js/main.js
+++ js/main.js
@@ -6,10 +6,12 @@ var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
   modal.style.display = "block";
+  $("body").addClass("modal-open");
 }
 
 span.onclick = function() {
   modal.style.display = "none";
+  $("body").removeClass("modal-open")
 }
 
 window.onclick = function(event) {
@@ -17,19 +19,3 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    touchDrag: false,
-    mouseDrag: false,
-    nav: true, // Show next and prev buttons
-    slideSpeed : 300,
-    paginationSpeed : 400,
-    autoPlay: true,
-    autoPlay: 4000,
-    items: 1,
-    animateIn: 'fadeIn',
-    animateOut: 'fadeOut'
-  });
-  $('.owl-carousel').find('.owl-nav').removeClass('disabled');
-});
