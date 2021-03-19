1b52e9a5c2125685ea234f56ba77299c9096e735
Oleg <O.Tseiko@gmail.com>
2021-03-19 14:33:39 +0200

    finished form

--- js/main.js
+++ js/main.js
@@ -20,7 +20,34 @@ window.onclick = function(event) {
     $("body").removeClass("modal-open");
   }
 }
-function onInvalidName() {
-  var self = $(this);
-  self.css('border-color', 'red');
-}
+
+$("#sendToVal").click(function() {
+  let name= $(".inpName").val();
+  let telephone= $(".inpTel").val();
+  let email= $(".inpMail").val();
+  let nameReg = /^[a-zA-Z\u0401\u0451\u0410-\u044f]{2,40}$/;
+  let regMail = /^\S+@\S+\.\S+$/;
+  let regPhone = /^[0-9\-\+]{9,15}$/;
+  if (name==null || name=="" || name.search(nameReg)){
+    $(".inpName").attr("placeholder", "Поле обязательно до заполнения").val("");
+    $(".inpName").addClass("wrong_inp");
+    $(".closed").removeClass("sended");
+    return false;
+  } else if(telephone.search(regPhone)){
+    $(".inpName").removeClass("wrong_inp");
+    $(".inpTel").attr("placeholder", "Поле обязательно до заполнения").val("");
+    $(".inpTel").addClass("wrong_inp");
+    $(".closed").removeClass("sended");
+    return false;
+  } else if (email.search(regMail)) {
+    $(".inpTel").removeClass("wrong_inp");
+    $(".inpMail").attr("placeholder", "Поле обязательно до заполнения").val("");
+    $(".inpMail").addClass("wrong_inp");
+    $(".closed").removeClass("sended");
+    return  false;
+  } else {
+    $(".inpMail").removeClass("wrong_inp");
+    $(".closed").addClass("sended");
+    return true;
+  }
+});
